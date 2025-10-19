51c5cb4c9140f2b8f8b83c6482a7d4f0eeb217af
sahm709 <nadhirsyah@gmail.com>
2025-10-19 22:28:40 +0700

    fix settings

--- perpus_web/settings.py
+++ perpus_web/settings.py
@@ -85,13 +85,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'perpus_web.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
