b09af574eaf398af11d4a050e33cd51f9ae7b099
unknown <nadhirsyah@gmail.com>
2025-10-21 12:04:46 +0700

    fix double message

--- library/models.py
+++ library/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
+    year = models.IntegerField(default=0)
     is_available = models.BooleanField(default=True)  # True = Tersedia
     added_date = models.DateTimeField(auto_now_add=True)  # Untuk sort buku baru
     is_recommended = models.BooleanField(default=False)
